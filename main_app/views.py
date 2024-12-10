81f4c47e4295421488b43b2f919721ba9e5c9d59
JakeMackBrown <JakeMackBrown@gmail.com>
2024-12-10 14:06:06 -0600

    rendering data in template (still rough)

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
-
 def home(request):
-    return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def event_index(request):
+    return render(request, 'events/index.html', {'events': events})
+
+
+class Event:
+    def __init__(self, title, description, date, location, category, tags):
+        self.title = title
+        self.description = description
+        self.date = date
+        self.location = location
+        self.category = category
+        self.tags = tags
+
+# Create a list of Cat instances
+events = [
+    Event('Machine Girl concert', 'Machine Girl live in concert for their new album', 'October 25th', 'The Firebird', 'Concert', 'Electronic, Grungy, Videogame music')
+]
